14244d8df2cf41d92f552ad276fd93618a67e327
Konkitoman <konkitoman@users.noreply.github.com>
2023-04-28 15:56:56 +0300

    Cleanup

--- examples/server.rs
+++ examples/server.rs
@@ -1,23 +1,13 @@
 use std::time::Duration;
 
-use relay_man::server::{ClientStage, RelayServer};
+use relay_man::server::RelayServer;
 
 fn main() {
     env_logger::init();
     let mut server = RelayServer::new("0.0.0.0", Duration::from_secs(5)).unwrap();
-    println!("Server Created");
+    println!("Server Started");
 
     loop {
-        std::thread::sleep(Duration::from_millis(0));
         server.step();
-        println!("L:");
-        for client in server.clients.iter() {
-            if let ClientStage::Registered(rclient) = &client.stage {
-                println!(
-                    "    Client: {}, session: {} adress: {:?}, ports: {:?}, to_connect: {:?}",
-                    rclient.name, client.session, rclient.adress, rclient.ports, rclient.to_connect
-                );
-            }
-        }
     }
 }
